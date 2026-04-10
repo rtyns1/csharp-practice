97023dde17bae3f2e473fd4c4531de1133dec58a
Karel Martyns <martynskarel@gmail.com>
2026-04-10 23:55:50 +0300

    initial commit

---
+++ General/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,14 @@
+﻿using ReverseString;
+using System;
+
+namespace ReverseStrnig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reverser = new StringReverser();
+            reverser.Run();
+        }
+    }
+}
---
+++ General/ReverseString/ReverseString/StringReverser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class StringReverser
+    {
+        public void Run()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            string reversed = ReverseString(input);
+
+            Console.WriteLine("Reversed string: " + reversed);
+        }
+
+        private string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] result = new char[input.Length];
+            int j = 0;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                result[j] = input[i];
+                j++;
+            }
+
+            return new string(result);
+        }
+    }
+}
