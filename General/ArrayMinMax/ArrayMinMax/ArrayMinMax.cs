d1df967cbe2dc8cde4d5ed3f0f65f666a6134c48
Karel Martyns <martynskarel@gmail.com>
2026-04-12 01:28:49 +0300

    Completed the ArrayMinMax console app, and wrote a readme markdown file for it

---
+++ General/ArrayMinMax/ArrayMinMax/ArrayMinMax.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+
+
+namespace ArrayMinMax
+{
+    public class ArrayMinMax
+    {
+        //first step as always is to aim to understand the problem
+        // so we take user input, make sure they are numeric, then put them in an array
+        // then we find the min and max of the array and print them out
+        public void GetMinMax()
+        {
+            Console.WriteLine("Enter a list of numbers separated by commas: ");
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Input cannot be empty.Try again.");
+                return;
+            }
+
+
+            string[] numbers = input.Split(',');
+            List<int> validNumbers = new List<int>();
+            bool allValid = true;
+
+            foreach (string number in numbers)
+            {
+                if (int.TryParse(number.Trim(), out int validNumber))
+                {
+                    validNumbers.Add(validNumber);
+                }
+                else
+                {
+                    allValid = false;
+                    Console.WriteLine($"'{number.Trim()}' is not a valid number.");
+                }
+
+            }
+
+            int[] Numbers = validNumbers.ToArray();
+
+            int max = Numbers[0];
+            int min = Numbers[0];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] > max)
+                {
+                    max = Numbers[i];
+                }
+
+                if (Numbers[i] < min)
+                {
+                    min = Numbers[i];
+
+                }
+
+            }
+
+
+            Console.WriteLine($"The maximum number is {max}");
+            Console.WriteLine($"The minimum number is {min}");
+
+
+
+        }
+
+
+    }
+}
---
+++ General/ArrayMinMax/ArrayMinMax/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayMinMax arrayMinMax = new ArrayMinMax();
+            arrayMinMax.GetMinMax();
+
+
+        }
+    }
+}
