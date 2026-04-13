5652cdb6cc48d19b6fab8e6a0e7825085f3ae3e9
Karel Martyns <martynskarel@gmail.com>
2026-04-13 12:08:29 +0300

    Completed the code in the Models folder, turns out, they are all just raw data containers.

--- Backend practice/AsyncDataAggregator--Backend practice 1/AsyncDataAggregator--Backend practice 1/MODELS/AggregateData.cs
+++ Backend practice/AsyncDataAggregator--Backend practice 1/AsyncDataAggregator--Backend practice 1/MODELS/AggregateData.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AsyncDataAggregator__Backend_practice_1.MODELS
+namespace AsyncDataAggregator__Backend_practice_1.Models
 {
-    internal class AggregateData
+    public class AggregateData
     {
+        // combination from the 3 APIs, returns one row of final output combining one user with prayer times
+        public string? UserName { get; set; }
+        public string? Fajr { get; set; }
+        public string? Dhuhr { get; set; }
+        public string? Asr { get; set; }
+        public string? Maghrib { get; set; }
+        public string? Isha { get; set; }
     }
 }
